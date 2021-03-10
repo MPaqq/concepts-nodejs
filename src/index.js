74d8b132cb69c028350e147689c18307d7069828
lemuelZara <lemuel.czara@gmail.com>
2021-03-10 12:07:01 -0300

    feat: create middleware to check if exists user account

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.some(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: `User doesn't exists!` })
+  }
+
+  request.username = username;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
