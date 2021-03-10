7030d5b3aa452469a5e15372d7a1a74575e04393
lemuelZara <lemuel.czara@gmail.com>
2021-03-10 12:23:19 -0300

    feat: implementation logic to add new todo

--- src/index.js
+++ src/index.js
@@ -49,7 +49,22 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request;
+
+  const user = users.find(user => user.username === username);
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
